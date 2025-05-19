6b51e7b84537725b3362f271bdd9bf0797f35454
CelinaBenedito <celina.benedito@sptech.school>
2025-05-19 14:34:19 -0300

    adição de arquivos

--- public/js/modais.js
+++
@@ -1,8 +0,0 @@
-function abrirGarras() {
-    garras.showModal()
-    container_garras.innerHTML = `Funciona oh meu deus
-    <button onclick="fecharGarras()">Fechar</button>`
-}
-function fecharGarras() {
-    garras.close()
-}
