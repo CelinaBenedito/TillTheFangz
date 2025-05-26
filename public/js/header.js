b22c2e41924b7eb138b40055fc5fa0837f398f74
CelinaBenedito <celina.benedito@sptech.school>
2025-05-26 20:41:23 -0300

    remoção do quiz

--- public/js/header.js
+++ public/js/header.js
@@ -9,7 +9,6 @@ if(sessionStorage.ID_USUARIO != undefined){
             <a href="../index.html">Home</a>
             <a href="livro.html">Livro</a>
             <a href="bestiario.html">Bestiário</a>
-            <a href="quiz.html">Quiz</a>
             <a href="ficha.html">Ficha</a>
             <a href="feed.html">Feed</a>
             <a href="user.html" id="imagem_usuario_header" class="img_User"><img src="assets/img/vampireUser.png" alt="user"></a>
@@ -27,7 +26,6 @@ else{
       <a href="../index.html">Home</a>
       <a href="../livro.html">Livro</a>
       <a href="../bestiario.html">Bestiário</a>
-      <a href="../quiz.html">Quiz</a>
       <a href="../ficha.html">Ficha</a>
       <a href="../feed.html">Feed</a>
       <a href="../login.html" class="login"><img src="../assets/img/vampireUser.png" alt="user"> Login</a>
